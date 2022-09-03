234ac65e9591ca38b4d81816f06c6e361f8918bf
Sid <sidbc@pm.me>
2022-09-03 12:14:23 +1200

    Swoosh attack left and right

--- Player.gd
+++ Player.gd
@@ -11,30 +11,51 @@ const JUMP_SPEED = 200
 
 var velocity = Vector2()
 var jumping = false
+var facing_right = true
+var attack_right = false
+var attack_left = false
 
 func _physics_process(delta):
 	velocity.y += delta * GRAVITY
 
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -WALK_SPEED
-		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.animation = 'walking'
-		$AnimatedSprite.playing = true
+		$Hero.animation = 'walking'
+		$Hero.playing = true
+		facing_right = false
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x =  WALK_SPEED
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.animation = 'walking'
-		$AnimatedSprite.playing = true
+		$Hero.animation = 'walking'
+		$Hero.playing = true
+		facing_right = true
 	else:
 		velocity.x = 0
-		$AnimatedSprite.playing = true
-		$AnimatedSprite.animation = 'idle'
+		$Hero.playing = true
+		$Hero.animation = 'idle'
+
+	if facing_right:
+		$Hero.flip_h = false
+	else:
+		$Hero.flip_h = true
+		
 
 	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
 
 	# The second parameter of "move_and_slide" is the normal pointing up.
 	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
 	move_and_slide(velocity, Vector2(0, -1))
+	
+	if !attack_right and !attack_left and Input.is_action_just_pressed("ui_attack"):
+		if facing_right:
+			$AttackRight.visible = true
+			attack_right = true
+			$AttackRight.frame = 0
+			$AttackRight.play("swoosh")
+		else:
+			$AttackLeft.visible = true
+			attack_left = true
+			$AttackLeft.frame = 0
+			$AttackLeft.play("swoosh")
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
@@ -43,6 +64,7 @@ func _physics_process(delta):
 		else:
 			jumping = false
 
+	
 # Called when the node enters the scene tree for the first time.
 #func _ready():
 #	pass # Replace with function body.
@@ -52,3 +74,14 @@ func _physics_process(delta):
 #func _process(delta):
 #	pass
 
+
+
+func _on_AttackRight_animation_finished():
+	attack_right = false
+	$AttackRight.visible = false
+	$AttackRight.stop()
+
+func _on_AttackLeft_animation_finished():
+	attack_left = false
+	$AttackLeft.visible = false
+	$AttackLeft.stop()
